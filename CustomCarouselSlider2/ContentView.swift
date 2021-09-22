76905a4ca082ec0c6f211bdc89ff7baa3c9afce8
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-09-22 12:25:26 +0600

    Initial Commit

---
+++ CustomCarouselSlider2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomCarouselSlider2
+//
+//  Created by Sopnil Sohan on 22/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomCarouselSlider2/CustomCarouselSlider2App.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCarouselSlider2App.swift
+//  CustomCarouselSlider2
+//
+//  Created by Sopnil Sohan on 22/9/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomCarouselSlider2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
